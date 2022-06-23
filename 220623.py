bbe974902e9cf6372149639d5c93df03a4c2e8cb
Management001 <44454495+Management001@users.noreply.github.com>
2022-06-23 23:10:25 +0900

    Create 220623.py

---
+++ 220623.py
@@ -0,0 +1,115 @@
+```python
+print("Hello World")
+print("Mary\'s cosmetics")
+print("신씨가 소리질렀다. \"도둑이야\".")
+print("\"C:\\Windows\"")
+print("안녕하세요.\n만나서\t\t반갑습니다.")
+# \t 는 탭을 의미하며, \n 는 엔터를 의미합니다.
+print ("오늘은","일요일")
+print("naver;kakao;sk;samsung")
+print("naver","kakao","sk","samsung",sep=";")
+print("naver","kakao","sk","samsung",sep="\\")
+print("first", end="");print("second")
+print(5/3)
+
+# 011 ~ 020
+
+samsung = 50000
+print("삼성전자 주가 : "+str(samsung))
+take=10
+print("삼성전자 주가 보유량 : "+str(take))
+print("삼성전자 보유 총 가치 : "+str(samsung*take))
+
+삼성전자 = 3000
+print(삼성전자)
+
+###
+
+
+시가총액 = 298E10
+현재가 = 50000
+PER = 15.79
+
+print("{0:7} {1:7}".format("항목","값"))
+print("시가총액",float(시가총액))
+print("현재가",int(현재가))
+print("PER",float(PER))
+
+
+
+###
+
+
+s = "hello"
+t = "python"
+
+print("실행 예:",s+"! "+t)
+
+
+###
+
+print(2+2*3)
+
+###
+
+a = "132"
+print(type(a))
+
+###
+
+num = 100
+result=str(num)
+print(result)
+
+###
+
+type1 = "15.79"
+print(float(type1))
+
+###
+
+year = "2020"
+int_year = int(year)
+print(int_year+3, int_year+2, int_year+1, int_year, int_year-1, int_year-2, int_year-3)
+
+###
+
+print(48584*36)
+
+### 
+
+##### 021 ~ 030
+
+###
+
+letters = 'python'
+print(letters[0],letters[2])
+
+###
+
+license_plate = "24가 2210"
+print(license_plate[4:])
+
+###
+
+string1 = "홀짝홀짝홀짝"
+print(string1[::2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+```
